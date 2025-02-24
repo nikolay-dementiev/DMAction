19613492eda59146a5bd9fc7a167086d0742840b
nikolay dementiev <nikolay@medisafe.com>
2025-02-24 12:59:58 +0100

    add object documentary description

--- Sources/DMAction/DMAction+AttempCount.swift
+++ Sources/DMAction/DMAction+AttempCount.swift
@@ -6,25 +6,41 @@
 
 import Foundation
 
+/// Protocol for result values of `DMAction` that can be copied and have an optional attempt count.
 public protocol DMActionResultValueProtocol: Copyable {
     var attemptCount: UInt? { get }
 }
 
+/// Extension to provide a default implementation of `attemptCount` for `DMActionResultValueProtocol`.
 public extension DMActionResultValueProtocol {
     var attemptCount: UInt? { nil }
 }
 
+/// Extension for `Result` where the success type conforms to `Copyable` and the failure type is `Error`.
 public extension Result where Success: Copyable, Failure == Error {
-    ///Get rid from wrapper
-    ///return original result value that was passed via ActionType' completion closure
+    /// Unwrap the original result value that was passed via `ActionType`'s completion closure.
+    ///
+    /// - Returns: The original result value without any wrapper.
+    ///
+    /// Example:
+    ///
+    /// ```swift
+    /// let result: Result<Copyable, Error> = // Your Result instance
+    /// let unwrappedResult = result.unwrapValue()
+    /// ```
     func unwrapValue() -> DMAction.ResultType {
         self.map(unwrapNestedResult)
     }
     
+    /// The attempt count of the result.
     internal var attemptCount: UInt? {
         (try? self.get() as? DMActionResultValue)?.attemptCount
     }
     
+    /// Unwrap nested `DMActionResultValue` to get the original result value.
+    ///
+    /// - Parameter value: The value to unwrap.
+    /// - Returns: The original result value without any wrapper.
     private func unwrapNestedResult(_ value: Copyable) -> Copyable {
         var unwrappedValue: Copyable = value
         
@@ -37,7 +53,22 @@ public extension Result where Success: Copyable, Failure == Error {
     }
 }
 
+/// Internal extension for `DMAction` to map result with attempt count and unwrap nested results.
+/// Used in the SDK itself.
 internal extension DMAction {
+    /// Map result with the attempt count.
+    ///
+    /// - Parameters:
+    ///   - result: The result to map.
+    ///   - attempt: The attempt count.
+    /// - Returns: The mapped result with the attempt count.
+    ///
+    /// Example:
+    ///
+    /// ```swift
+    /// let result: DMAction.ResultType = // Your Result instance
+    /// let mappedResult = type(of: result).mapResultWithAttempt(result, attempt: 1)
+    /// ```
     static func mapResultWithAttempt(_ result: ResultType, attempt: UInt) -> ResultType {
         result.map {
             let valueWithoutWrapper = unwrapNestedResult($0)
@@ -45,6 +76,17 @@ internal extension DMAction {
         }
     }
     
+    /// Unwrap nested `DMActionResultValue` to get the original result value.
+    ///
+    /// - Parameter value: The value to unwrap.
+    /// - Returns: The original result value without any wrapper.
+    ///
+    /// Example:
+    ///
+    /// ```swift
+    /// let value: Copyable = // Your Copyable instance
+    /// let unwrappedValue = type(of: value).unwrapNestedResult(value)
+    /// ```
     static func unwrapNestedResult(_ value: Copyable) -> Copyable {
         var unwrappedValue: Copyable = value
         
--- Sources/DMAction/DMActionWithFallback.swift
+++ Sources/DMAction/DMActionWithFallback.swift
@@ -6,12 +6,23 @@
 
 import Foundation
 
+/// A struct representing an action with a fallback that conforms to the `DMAction` protocol.
 public struct DMActionWithFallback: DMAction {
+    /// The current attempt number of the action.
     public let currentAttempt: UInt
+    
+    /// The unique identifier of the action.
     public let id: UUID = UUID()
+    
+    /// The action to be performed.
     public let action: ActionType
     
-    // Initialize with primary and fallback actions
+    /// Initializes a new instance of `DMActionWithFallback` with the specified primary and fallback actions.
+    ///
+    /// - Parameters:
+    ///   - currentAttempt: The current attempt number.
+    ///   - primaryAction: The primary action to be performed.
+    ///   - fallbackAction: The fallback action to be performed if the primary action fails.
     public init(currentAttempt: UInt,
                 _ primaryAction: @escaping ActionType,
                 _ fallbackAction: @escaping ActionType) {
