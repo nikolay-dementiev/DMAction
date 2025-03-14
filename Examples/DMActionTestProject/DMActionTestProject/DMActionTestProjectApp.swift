02ae8438d8d1c456e3b822695a9fc03f593cd33c
nikolay dementiev <nikolas.dementiev@gmail.com>
2025-03-14 10:43:49 +0100

    add test project as an Example

---
+++ Examples/DMActionTestProject/DMActionTestProject/DMActionTestProjectApp.swift
@@ -0,0 +1,16 @@
+//
+//  DMActionTestProject
+//
+//  Created by Mykola Dementiev
+//
+
+import SwiftUI
+
+@main
+struct DMActionTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
